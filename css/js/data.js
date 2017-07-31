e4ea9c13d7b0e664fdf24c474a41a4efcd117454
don79is <don79is@gmail.com>
2017-07-31 14:31:00 +0300

    put  data.js in js folder

---
+++ css/js/data.js
@@ -0,0 +1,104 @@
+var buttonArray = [
+    {
+        "type": "action",
+        "value": "C",
+        "buttonClass": "btn btn-success"
+    },
+    {
+        "type": "action",
+        "value": "<<",
+        "buttonClass": "btn btn-success"
+    },
+    {
+        "type": "action",
+        "value": "%",
+        "buttonClass": "btn btn-success off"
+    },
+    {
+        "type": "action",
+        "value": "/",
+        "buttonClass": "btn btn-success off"
+    },
+    {
+        "type": "number",
+        "value": 9,
+        "buttonClass": "btn btn-default"
+    },
+    {
+        "type": "number",
+        "value": 8,
+        "buttonClass": "btn btn-default"
+    },
+    {
+        "type": "number",
+        "value": 7,
+        "buttonClass": "btn btn-default"
+    },
+    {
+        "type": "action",
+        "value": "*",
+        "buttonClass": "btn btn-success off"
+    },
+    {
+        "type": "number",
+        "value": 4,
+        "buttonClass": "btn btn-default"
+    },
+    {
+        "type": "number",
+        "value": 5,
+        "buttonClass": "btn btn-default"
+    },
+    {
+        "type": "number",
+        "value": 6,
+        "buttonClass": "btn btn-default"
+    },
+    {
+        "type": "action",
+        "value": "-",
+        "buttonClass": "btn btn-success off"
+    },
+    {
+        "type": "number",
+        "value": 1,
+        "buttonClass": "btn btn-default"
+    },
+    {
+        "type": "number",
+        "value": 2,
+        "buttonClass": "btn btn-default"
+    },
+    {
+        "type": "number",
+        "value": 3,
+        "buttonClass": "btn btn-default"
+    },
+    {
+        "type": "action",
+        "value": "+",
+        "buttonClass": "btn btn-success off"
+    },
+    {
+        "type": "action",
+        "value": "+-",
+        "buttonClass": "btn btn-success off"
+    },
+    {
+        "type": "number",
+        "value": 0,
+        "buttonClass": "btn btn-default"
+    },
+    {
+        "type": "number",
+        "value": ".",
+        "buttonClass": "btn btn-success"
+    },
+    {
+        "type": "action",
+        "value": "=",
+        "buttonClass": "btn btn-danger off"
+    }
+
+
+];
