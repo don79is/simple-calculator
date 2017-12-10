eceea118520792818fdb306ddd2041edc3a378e3
Donatas Tumanas <donatastum@gmail.com>
2017-12-10 20:10:24 +0200

    update js add materialize

--- css/js/data.js
+++ css/js/data.js
@@ -2,103 +2,110 @@ var buttonArray = [
     {
         "type": "action",
         "value": "C",
-        "buttonClass": "btn btn-success"
+        "buttonClass": "waves-effect waves-black btn red darken-2"
     },
     {
         "type": "action",
-        "value": "<<",
-        "buttonClass": "btn btn-success"
+        "value": "CE",
+        "buttonClass": "waves-effect waves-black btn orange"
     },
     {
         "type": "action",
         "value": "%",
-        "buttonClass": "btn btn-success off"
+        "buttonClass": "waves-effect waves-black btn orange off"
     },
     {
         "type": "action",
         "value": "/",
-        "buttonClass": "btn btn-success off"
+        "buttonClass": "waves-effect waves-black btn orange off"
     },
     {
         "type": "number",
         "value": 9,
-        "buttonClass": "btn btn-default"
+        "buttonClass": "waves-effect waves-black btn white"
     },
     {
         "type": "number",
         "value": 8,
-        "buttonClass": "btn btn-default"
+        "buttonClass": "waves-effect waves-black btn white"
     },
     {
         "type": "number",
         "value": 7,
-        "buttonClass": "btn btn-default"
+        "buttonClass": "waves-effect waves-black btn white"
     },
     {
         "type": "action",
         "value": "*",
-        "buttonClass": "btn btn-success off"
+        "buttonClass": "waves-effect waves-black btn orange off"
     },
     {
         "type": "number",
         "value": 4,
-        "buttonClass": "btn btn-default"
+        "buttonClass": "waves-effect waves-black btn white"
     },
     {
         "type": "number",
         "value": 5,
-        "buttonClass": "btn btn-default"
+        "buttonClass": "waves-effect waves-black btn white"
     },
     {
         "type": "number",
         "value": 6,
-        "buttonClass": "btn btn-default"
+        "buttonClass": "waves-effect waves-black btn white"
     },
     {
         "type": "action",
         "value": "-",
-        "buttonClass": "btn btn-success off"
+        "buttonClass": "waves-effect waves-black btn orange off"
     },
     {
         "type": "number",
         "value": 1,
-        "buttonClass": "btn btn-default"
+        "buttonClass": "waves-effect waves-black btn white"
     },
     {
         "type": "number",
         "value": 2,
-        "buttonClass": "btn btn-default"
+        "buttonClass": "waves-effect waves-black btn white"
     },
     {
         "type": "number",
         "value": 3,
-        "buttonClass": "btn btn-default"
+        "buttonClass": "waves-effect waves-black btn white"
     },
     {
         "type": "action",
         "value": "+",
-        "buttonClass": "btn btn-success off"
+        "buttonClass": "waves-effect waves-black btn orange off"
     },
     {
         "type": "action",
         "value": "+-",
-        "buttonClass": "btn btn-success off"
+        "buttonClass": "waves-effect waves-black btn orange off"
     },
     {
         "type": "number",
         "value": 0,
-        "buttonClass": "btn btn-default"
+        "buttonClass": "waves-effect waves-black btn white"
     },
     {
         "type": "number",
         "value": ".",
-        "buttonClass": "btn btn-success"
+        "buttonClass": "waves-effect waves-black btn orange"
     },
     {
         "type": "action",
         "value": "=",
-        "buttonClass": "btn btn-danger off"
+        "buttonClass": "waves-effect waves-black btn blue off"
     }
 
 
 ];
+
+var ACTION_INCREMENT = 'INCRE',
+    ACTION_DELETE = 'DELETE',
+    ACTION_PLUSMINUS = '+-',
+    ACTION_CALCUL = 'calcul',
+    ACTION_DELETE_LAST = 'CE',
+    ACTION_PER = '%';
--- css/js/script.js
+++ css/js/script.js
@@ -1,242 +1,224 @@
-/**
- * Created by Donatas on 2017-07-27.
- */
-var numbers = ['0'];
-var actions = [];
 
-var ACTION_INCREMENT = 'INCRE',
-    ACTION_DELETE = 'DELETE',
-    ACTION_PLUSMINUS = '+-',
-    ACTION_CALCULATE = 'CALKU',
-    ACTION_DELETE_LAST = '<<',
-    ACTION_PER = '%';
-
-// script creates html div tag, inserts input field and table of buttons
 $(document).ready(function () {
-    $('body').prepend("<div id='buttonField'></div>");
-
-    // change to read only, give value
-    $('#buttonField').append("<input style='color: ghostwhite' value='0' disabled>");
-    $('#buttonField').append("<br>");
-
+    $('body').prepend("<div class='calculator'></div>");
+    $('.calculator').append("<input style='color: floralwhite; font-size: 1.2em;' value='0' disabled>");
     $.each(buttonArray, function (index, buttonArray) {
-        $("#buttonField").append($("<button s>" + buttonArray.value + "</button>")
-            .attr("class", buttonArray.buttonClass).attr("type", buttonArray.type).attr("value", buttonArray.value));
+        $(".calculator").append($("<button>" + buttonArray.value + "</button>").attr("class", buttonArray.buttonClass).attr("type", buttonArray.type).attr("value", buttonArray.value).attr("disable", false)
+        );
     });
 });
 
-
-function updateInput() {
-    var res = '';
-    if (numbers.length === 1) {
-        res = numbers[0]
-    } else {
-        for (var i = 0; i < numbers.length; i++) {
-
-            if (numbers[i] !== '0') {
-                res += numbers[i];
-            }
-
-            if (actions[i]) {
-                res += "" + actions[i] + "";
-            }
-        }
-    }
-    $('input').val(res);
-    console.log(numbers, actions);
-}
-
-
+/*
+ * This function check type and do action by case*/
 function handleClick(e) {
     var $b = $(e.currentTarget);
-
-    function updateNumber(action, value) {
-        switch (action) {
-
-            case ACTION_INCREMENT:
-                var n = numbers [actions.length];
-
-                switch (value) {
-                    case '.':
-                        if (n.indexOf('.') === -1) {
-                            n += value;
-                        }
-                        break;
-
-                    case '0':
-                        if (n.length === 1 && n === '0') {
-                        }
-                        else {
-                            n += value;
-                        }
-                        break;
-
-                    default:
-                        if (n.length === 1 && n === '0')
-                            n = value;
-                        else {
-                            n += value;
-                        }
-                }
-                numbers [actions.length] = n;
-                break;
-
-            case ACTION_CALCULATE:
-                var a = parseFloat(numbers[0]);
-                var b;
-
-                for (var i = 1; i < numbers.length; i++) {
-
-                    b = parseFloat(numbers[i]);
-
-                    switch (actions[i - 1]) {
-                        case '+':
-                            a += b;
-                            break;
-
-                        case '-':
-                            a -= b;
-                            break;
-
-                        case '/':
-                            a /= b;
-                            break;
-
-                        case '*':
-                            a *= b;
-                            break;
-                    }
-                }
-                numbers = [a.toString()];
-                actions = [];
-                break;
-
-            case ACTION_PER:
-                var a = parseFloat(numbers[0]);
-                var b;
-
-
-                for (var i = 1; i < numbers.length - 1; i++) {
-
-                    b = parseFloat(numbers[i]);
-
-                    switch (actions[i - 1]) {
-                        case '+':
-                            a += b;
-                            break;
-
-                        case '-':
-                            a -= b;
-                            break;
-
-                        case '/':
-                            a /= b;
-                            break;
-
-                        case '*':
-                            a *= b;
-                            break;
-                    }
-                    console.log(a)
-                }
-
-                if (numbers.length === 1)
-                    numbers = ['0'];
-                else
-                    numbers[actions.length] = (numbers[actions.length] * (a / 100)).toString();
-                break;
-
-            case ACTION_DELETE_LAST:
-
-                if (numbers[actions.length] === '0') {
-
-                    if (numbers.length > 1) {
-                        numbers.pop();
-                        actions.pop();
-                    }
-                } else {
-                    numbers[actions.length] = numbers[actions.length].substring(0, numbers[actions.length].length - 1);
-                }
-                if (
-                    numbers[actions.length].length === 0)
-                    numbers[actions.length] = '0';
-                break;
-
-            case ACTION_DELETE:
-                $('input').val("0");
-                actions = [];
-                numbers = ['0'];
-                break;
-
-            case ACTION_PLUSMINUS:
-                if (numbers[actions.length][0] === '-') {
-                    numbers[actions.length] = numbers[actions.length].substring(1, numbers[actions.length].length);
-                } else {
-
-                    if (numbers[actions.length] !== '0') {
-                        numbers[actions.length] = '-' + numbers[actions.length];
-                    }
-                }
-                break;
-        }
-    }
-
     if ($b.attr('type') === 'number') {
-        updateNumber(ACTION_INCREMENT, $b.val())
-    }
-    else if ($b.attr('type') === 'action') {
+        updateNumber(ACTION_INCREMENT, $b.val());
+    } else {
         switch ($b.val()) {
+            case 'C':
+                updateNumber(ACTION_DELETE);
+                break;
+            case 'CE':
+                updateNumber(ACTION_DELETE_LAST);
+                break;
+            case '+-':
+                updateNumber(ACTION_PLUSMINUS);
+                break;
+            case '%':
+                updateNumber(ACTION_PER);
+                break;
 
-            case "+":
-            case "-":
-            case "*":
-            case "/":
-
+            //update action array and creating new number
+            case '+':
+            case '-':
+            case '*':
+            case '/':
                 var n = numbers[actions.length];
-
                 if (n[n.length - 1] === '.') {
                     n = n.substring(0, n.length - 1);
                 }
                 numbers[actions.length] = n;
-
                 if (numbers[numbers.length - 1] !== '0') {
                     actions.push($b.val());
-                    numbers [actions.length] = '0';
-                }
-                else {
+                    numbers[actions.length] = '0'
+                } else {
                     actions.pop();
                     actions.push($b.val());
-
                 }
+                numbers[actions.length] = '0';
                 break;
-
-            case 'C':
-                updateNumber(ACTION_DELETE);
-                break;
-
             case '=':
-                updateNumber(ACTION_CALCULATE);
-                break;
-
-            case "<<":
-                updateNumber(ACTION_DELETE_LAST);
-                break;
-
-            case"+-":
-                updateNumber(ACTION_PLUSMINUS, $b.val());
-                break;
-
-            case "%":
-                updateNumber(ACTION_PER);
+                updateNumber(ACTION_CALCUL);
                 break;
         }
     }
-    updateInput(numbers, actions);
+    updateInput();
 }
 
+var numbers = ['0'];
+var actions = [];
+
+/*
+ * That's all logic
+ * */
+
+function updateNumber(action, value) {
+    switch (action) {
+        case ACTION_INCREMENT:
+            var n = numbers[actions.length];
+
+            switch (value) {
+                case '.':
+                    n += value;
+                    if (n.indexOf('.') === -1) {
+                    }
+                    break;
+
+                case '0':
+                    if (n.length === 1 && n === '0') {
+                    } else {
+                        n += value;
+                    }
+                    break;
+
+                    break;
+                default:
+                    if (n.length === 1 && n === '0') {
+                        n = value;
+                    } else {
+                        n += value;
+                    }
+            }
+            numbers[actions.length] = n;
+            break;
+
+        case ACTION_DELETE_LAST:
+            if (numbers[actions.length] === '0') {
+                if (numbers.length > 1) {
+                    numbers.pop();
+                    actions.pop();
+                }
+            } else {
+
+                n = numbers[actions.length];
+                n = n.substring(0, n.length - 1);
+
+                if (n.length === 0)
+                    n = '0';
+
+                numbers[actions.length] = n;
+            }
+            break;
+
+        case ACTION_DELETE:
+            numbers = ['0'];
+            actions = [];
+
+            break;
+
+        case ACTION_PLUSMINUS:
+            n = numbers[actions.length];
+            if (n[0] === '-') {
+                n = n.substring(1, n.length);
+            } else {
+                if (n !== '0') {
+                    n = '-' + n;
+                }
+            }
+            numbers[actions.length] = n;
+            break;
+
+        case ACTION_CALCUL:
+
+            var a;
+
+            for (var i = 0; i < numbers.length; i++) {
+                if (a) {
+                    var b = parseFloat(numbers[i]);
+                    switch (actions[i - 1]) {
+                        case '+':
+                            a += b;
+                            break;
+                        case '-':
+                            a -= b;
+                            break;
+                        case '*':
+                            a *= b;
+                            break;
+                        case '/':
+                            a /= b;
+                            break;
+                    }
+                } else {
+                    a = parseFloat(numbers[i]);
+                }
+            }
+
+            numbers = [a.toString()];
+            actions = [];
+            break;
+
+        case ACTION_PER:
+
+            var a;
+            if (numbers[action.length] === '0')
+                numbers = '0';
+            else {
+                for (var i = 0; i < numbers.length - 1; i++) {
+                    if (a) {
+                        var b = parseFloat(numbers[i]);
+                        switch (actions[i - 1]) {
+                            case '+':
+                                a += b;
+                                break;
+                            case '-':
+                                a -= b;
+                                break;
+                            case '*':
+                                a *= b;
+                                break;
+                            case '/':
+                                a /= b;
+                                break;
+
+                        }
+                    } else {
+                        a = parseFloat(numbers[i]);
+                    }
+                }
+                numbers[actions.length] = ((a / 100) * numbers[actions.length]).toString();
+
+                if (numbers[actions.length] === "NaN") {
+                    numbers[actions.length] = '0';
+                }
+
+            }
+            break;
+
+    }
+}
+
+/*
+ * * Show numbers and action, result to input*/
+function updateInput() {
+
+    var upIntp = '';
+    if (numbers.length === 1) {
+        upIntp = numbers['0'];
+    } else {
+        for (var i = 0; i < numbers.length; i++) {
+            if (numbers[i] !== '0')
+                upIntp += numbers[i];
+            if (actions[i]) {
+                upIntp += '' + actions[i] + '';
+            }
+        }
+    }
+
+    $('input').val(upIntp);
+}
 
 $(document).ready(function () {
     $("button").click(handleClick);
-
 });
-
